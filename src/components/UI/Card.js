1de9849a3128a4b1815dd346a293e2facd2dc128
Pranav <pranavmendi@gmail.com>
2021-04-25 06:36:23 +0530

    Add useState for username and age and perform two-way-binding

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -3,10 +3,7 @@ import classes from './Card.module.css';
 
 const Card = (props) => {
 	return (
-		// eslint-disable-next-line prettier/prettier
-		<div className={`${classes.card} ${props.className}`}>
-			{props.children}
-		</div>
+		<div className={`${classes.card} ${props.className}`}>{props.children}</div>
 	);
 };
 
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './AddUser.module.css';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 
 const AddUser = (props) => {
+	const [enteredUsername, setEnteredUsername] = useState('');
+	const [enteredAge, setEnteredAge] = useState('');
 	const addUserHandler = (event) => {
 		event.preventDefault();
+		if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+			return;
+		}
+		if (+enteredAge < 1) {
+			// forcing conversion of enteredAge to number
+			return;
+		}
+		console.log(enteredUsername, enteredAge);
+		setEnteredUsername('');
+		setEnteredAge('');
+	};
+
+	const usernameChangeHandler = (event) => {
+		setEnteredUsername(event.target.value);
+	};
+	const ageChangeHandler = (event) => {
+		setEnteredAge(event.target.value);
 	};
 
 	return (
 		<Card className={classes.input}>
 			<form onSubmit={addUserHandler}>
 				<label htmlFor='username'>Username</label>
-				<input id='username' type='text'></input>
+				<input
+					id='username'
+					type='text'
+					onChange={usernameChangeHandler}
+					value={enteredUsername}></input>
+				{/* two way binding */}
 				<label htmlFor='age'>Age (Years)</label>
-				<input id='age' type='number'></input>
+				<input
+					id='age'
+					type='number'
+					onChange={ageChangeHandler}
+					value={enteredAge}></input>
+				{/* two way binding */}
 				<Button type='submit'>Submit</Button>
 			</form>
 		</Card>
