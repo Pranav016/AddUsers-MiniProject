fdc2bc7dfbe890e97a6f892ed806898109ea2d17
Pranav <pranavmendi@gmail.com>
2021-04-25 06:46:23 +0530

    Add UserList component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AddUser from './components/Users/AddUser';
+import UserList from './components/Users/UserList';
 
 function App() {
 	return (
 		<div>
 			<AddUser />
+			<UserList users={[]} />
 		</div>
 	);
 }
