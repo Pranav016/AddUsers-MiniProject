6a045d95be685a06556a5043e9ac018107db81ea
Pranav <pranavmendi@gmail.com>
2021-04-26 07:53:37 +0530

    Use React Portal to bring overlay to the top of DOM tree

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,23 +1,45 @@
 import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
 import classes from './ErrorModal.module.css';
 import Card from './Card';
 import Button from './Button';
 
+const Backdrop = (props) => {
+	return <div className={classes.backdrop} onClick={props.onConfirm} />;
+};
+
+const ModalOverlay = (props) => {
+	return (
+		<Card className={classes.modal}>
+			<header className={classes.header}>
+				<h1>{props.title}</h1>
+			</header>
+			<div className={classes.content}>
+				<p>{props.message}</p>
+			</div>
+			<footer className={classes.actions}>
+				<Button onClick={props.onConfirm}>Okay</Button>
+			</footer>
+		</Card>
+	);
+};
+
 const ErrorModal = (props) => {
 	return (
 		<Fragment>
-			<div className={classes.backdrop} onClick={props.onConfirm} />
-			<Card className={classes.modal}>
-				<header className={classes.header}>
-					<h1>{props.title}</h1>
-				</header>
-				<div className={classes.content}>
-					<p>{props.message}</p>
-				</div>
-				<footer className={classes.actions}>
-					<Button onClick={props.onConfirm}>Okay</Button>
-				</footer>
-			</Card>
+			{/* we use react portal here */}
+			{ReactDOM.createPortal(
+				<Backdrop onConfirm={props.onConfirm} />,
+				document.getElementById('backdrop-root')
+			)}
+			{ReactDOM.createPortal(
+				<ModalOverlay
+					title={props.title}
+					message={props.message}
+					onConfirm={props.onConfirm}
+				/>,
+				document.getElementById('overlay-root')
+			)}
 		</Fragment>
 	);
 };
